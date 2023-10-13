78a07538a37718b47bff714152757bee7a13e58e
viperinewaif <139336874+viperinewaif@users.noreply.github.com>
2023-10-13 14:34:00 +0900

    Update Layout.tsx

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -13,7 +13,7 @@ const shareButtonStyles: ICommandBarStyles & IButtonStyles = {
       width: 86,
       height: 32,
       borderRadius: 4,
-      background: 'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)',
+      background: 'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #c3662d 33.63%, #2D87C3 70.31%, #8DDDD8 100%)',
     //   position: 'absolute',
     //   right: 20,
       padding: '5px 12px',
